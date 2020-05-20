f2a5e6b3bb8e446f09e828fcea9c6afd2bc7a070
joaosoarees <jsoareez@gmail.com>
2020-05-20 16:43:18 -0300

    atualização no banco

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -13,6 +13,7 @@ routes.post('/sessions', SessionController.create);
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
 
+
 routes.get('/profile', ProfileController.index);
 
 routes.get('/incidents', IncidentController.index);
